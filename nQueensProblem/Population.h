1e9caf830dae879bf8d26766a7c44db6c08ecb7b
a0915628 <a0915628@unet.univie.ac.at>
2016-01-08 16:56:41 +0100

    templates deleted population limit solved

--- nQueensProblem/Population.h
+++ nQueensProblem/Population.h
@@ -1,38 +1,184 @@
+//Not tested
+
 #include <iostream>
 #include <stdlib.h>
 
-template <int N>
+/*
+ * Class defines Population for the algorithm;
+ * template parameter N sets n in n-Queens Problem
+ */
 class Population {
 
-  template <int N>
-  struct ChessBoard {
-    int m_iFitness;
-    int m_rgQueenPositions[N-1];
+/*
+ * Indiviual of the Population
+ * m_iFitnness - Integer that represents the viability to use in Mutation/Crossover
+ * m_rgQueenPositions - Integer Array with positions of Queens
+ */
+struct ChessBoard {
 
-    ChessBoard(): m_iFitness(0) {
-      for(size_t i = 0; i < N; i++) {
-        m_rgQueenPositions[i] = std::rand() % N;
-      }
-    };
 
-    void fnCheckFitness() {
-      for(size_t i = 0; i < N; i++) {
-        for(size_t j = 0; j < N-i; j++) {
-          if(m_rgQueenPositions[i] == m_rgQueenPositions[j]) {
-            m_iFitness--;
-          }
+	ChessBoard::ChessBoard(size_t board)
+	{
+		boardSize = board;
+		m_rgQueenPositions = new int[boardSize];
+
+	}
+
+	ChessBoard::~ChessBoard()
+	{
+		delete m_rgQueenPositions;
+	}
+        int m_iFitness;
+		size_t boardSize;
+		int *m_rgQueenPositions;
+        /*
+         * Constructor - Randomizes position of queens
+         */
+        ChessBoard() {
+                m_iFitness = 0;
+                for(size_t i = 0; i < boardSize; i++) {
+                        m_rgQueenPositions[i] = rand() % boardSize;
+                }
         }
-      }
-    }
-  };
 
-  ChessBoard **m_rgcbPopulation;
+        /*
+         * Function to set the fitness var
+         */
+        void fnCheckFitness() {
+                std::cout << m_iFitness << std::endl;
+                m_iFitness -= fnCheckRow() + fnCheckDiag();
+                std::cout << m_iFitness << std::endl;
+        }
 
-public:
-Population<N>::Population() {
-  for(size_t i = 0; i < 50; i++) {
-    m_rgcbPopulation[i] = new ChessBoard;
-  }
-}
+        int fnCheckRow();
+        int fnCheckDiag(); // TODO: combine with fnCheckRow; OR use Threads?
 
 };
+
+/*
+ * Dynamic array that holds the Indiviuals (TODO: use vector instead?)
+ */
+ChessBoard **m_cbBestInd;
+const static size_t m_cMaxPopulation = 500;
+size_t boardSize;
+size_t m_cPopulationCount;
+ChessBoard **m_rgcbPopulation;
+
+public:
+Population(size_t, size_t);
+~Population();
+void fnRateFitness();
+void fnGetBestInd();
+void fnMutatePop();
+void fnCrossoverPop();
+virtual std::ostream& print(std::ostream& o) const;
+
+};
+
+
+ 
+Population ::~Population() {
+        for(size_t i = 0; i < m_cPopulationCount; i++) {
+                delete m_rgcbPopulation[i];
+        }
+        delete [] m_rgcbPopulation;
+        delete m_cbBestInd;
+}
+
+/*
+ * Constructor - Fills member Array with n (TODO: 50?!)
+ */
+Population ::Population(size_t ucInitialPopSize, size_t boardsize) {
+        m_rgcbPopulation = new ChessBoard *[ucInitialPopSize]; //array initialize with ucInitialPopSize
+        m_cPopulationCount = 0;
+		boardSize = boardsize;
+        std::cout << m_cPopulationCount << std::endl;
+        for(size_t i = 0; i < ucInitialPopSize; i++) {
+                m_rgcbPopulation[i] = new ChessBoard(boardSize);
+                std::cout << m_rgcbPopulation[i]->m_iFitness << std::endl;
+                //m_cPopulationCount++;
+        }
+        m_cPopulationCount = ucInitialPopSize;
+
+        std::cout << ucInitialPopSize << std::endl;
+}
+
+/*
+ * Rates Fitness for Individuals in Population
+ */
+void Population ::fnRateFitness() {
+
+    std::cout << m_cPopulationCount << std::endl;
+
+        for(size_t i = 0; i < m_cPopulationCount; i++) {
+            std::cout << m_rgcbPopulation[i]->m_iFitness << std::endl;
+            std::cout << i << std::endl;
+                m_rgcbPopulation[i]->fnCheckFitness();
+        }
+
+}
+
+/*
+ * Checks if a queen is the only one in a vertical/horizontal row
+ * counter to modify fitness var in Individuals
+ */
+int Population ::ChessBoard::fnCheckRow() {
+        int i_cFitCountRow = 0;
+
+        for(size_t i = 0; i < boardSize; i++) {
+                for(size_t j = i+1; j < boardSize-i; j++) {
+                        if(m_rgQueenPositions[i] == m_rgQueenPositions[j]) {
+                                i_cFitCountRow++;
+                        }
+                }
+        }
+
+        return i_cFitCountRow;
+}
+
+/*
+ * Checks if a queen is the only one in a diagonal row
+ * counter to modify fitness var in Individuals
+ */
+int Population ::ChessBoard::fnCheckDiag() {
+        int i_cFitCountDiag = 0;
+
+        for(size_t i = 0; i < boardSize; i++) {
+                for(size_t j = i+1; j < boardSize-i; j++) {
+                        if(m_rgQueenPositions[j] == m_rgQueenPositions[i]+j || m_rgQueenPositions[j] == m_rgQueenPositions[i]-j) {
+                                i_cFitCountDiag++;
+                        }
+                }
+        }
+
+        return i_cFitCountDiag;
+}
+
+ 
+void Population ::fnGetBestInd() {
+    
+}
+
+ 
+void Population ::fnMutatePop() {
+
+}
+
+ 
+void Population ::fnCrossoverPop() {
+
+}
+
+std::ostream& Population ::print(std::ostream& o) const {
+        o << "Population, N = " << boardSize << ", popCount = " << m_cPopulationCount <<  '\n';
+        for(size_t i = 0; i < m_cPopulationCount; i++) {
+                o << '\n';
+                o << "Individual " << i << ", fitness = " << m_rgcbPopulation[i]->m_iFitness << '\n';
+                o << "QueensPos: [";
+                for(size_t j = 0; j < boardSize; j++) {
+                        o << m_rgcbPopulation[i]->m_rgQueenPositions[j] << ' ';
+                } o << ']' << '\n';
+        }
+
+        return o;
+}
--- nQueensProblem/nQueensProblem.cpp
+++ nQueensProblem/nQueensProblem.cpp
@@ -1,12 +1,29 @@
 // nQueensProblem.cpp : Defines the entry point for the console application.
 //
+#ifndef POPULATION_H
+#define POPULATION_H
 
 #include <iostream>
 #include "Population.h"
 
-int main()
-{
+int main() {
+
+        Population* p = new Population(30, 8);
+        /*const char helpStr[] =
+           "This is a Helperstr\n"
+           "It is here to help.\n";
+         */
+        std::cout << "alls gucci1" << std::endl;
+        p->fnRateFitness();
+        std::cout << "alls gucci2" << std::endl;
+        p->print(std::cout);
+        std::cout << "alls gucci3" << std::endl;
+		int no;
+		std::cin >> no;
 
 
-  return 0;
+        return 0;
+
 }
+
+#endif //POPULATION_H
