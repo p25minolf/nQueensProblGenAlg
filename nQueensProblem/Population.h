80efa8431b3353a1cdbc9e67a2726f413a86fe66
a0915628 <a0915628@unet.univie.ac.at>
2016-01-09 19:22:19 +0100

    insertionsort

--- nQueensProblem/Population.h
+++ nQueensProblem/Population.h
@@ -1,177 +1,165 @@
-//Not tested
-
 #include <iostream>
 #include <stdlib.h>
 #include <ctime>
 
 /*
- * Class defines Population for the algorithm;
- * template parameter N sets n in n-Queens Problem
- */
+* Class defines Population for the algorithm;
+* template parameter N sets n in n-Queens Problem
+*/
 class Population {
 
-/*
- * Indiviual of the Population
- * m_iFitnness - Integer that represents the viability to use in Mutation/Crossover
- * m_rgQueenPositions - Integer Array with positions of Queens
- */
-struct ChessBoard {
-
 	/*
-	* Constructor - Randomizes position of queens
+	* Indiviual of the Population
+	* m_iFitnness - Integer that represents the viability to use in Mutation/Crossover
+	* m_rgQueenPositions - Integer Array with positions of Queens
 	*/
-	ChessBoard(size_t board)
-	{
-		m_boardSize = board;
-		m_rgQueenPositions = new int[m_boardSize];
-		m_iFitness = 0;
-		for (size_t i = 0; i < m_boardSize; i++) {
-			m_rgQueenPositions[i] = rand() % m_boardSize;
-		}
-	}
+	struct ChessBoard {
 
-	~ChessBoard()
-	{
-		delete m_rgQueenPositions;
-	}
-        int m_iFitness;
+		/*
+		* Constructor - Randomizes position of queens
+		*/
+		ChessBoard(size_t board)
+		{
+			m_boardSize = board;
+			m_rgQueenPositions = new int[m_boardSize];
+			m_iFitness = 0;
+			for (size_t i = 0; i < m_boardSize; i++) {
+				m_rgQueenPositions[i] = rand() % m_boardSize;
+			}
+		}
+
+		~ChessBoard()
+		{
+			delete m_rgQueenPositions;
+		}
+		int m_iFitness;
 		size_t m_boardSize;
 		int *m_rgQueenPositions;
 
 
 
-        /*
-         * Function to set the fitness var
-         */
-        void fnCheckFitness() {
+		/*
+		* Function to set the fitness var
+		*/
+		void fnCheckFitness() {
 			m_iFitness = fnCheck();
-        }
-
-        int fnCheck();
-
-};
-
-/*
- * Dynamic array that holds the Indiviuals (TODO: use vector instead?)
- */
-ChessBoard **m_cbBestInd;
-const static size_t m_cMaxPopulation = 500;
-size_t m_boardSize;
-size_t m_cPopulationCount;
-size_t m_bestPop;
-ChessBoard **m_rgcbPopulation;
-
-public:
-Population(size_t, size_t);
-~Population();
-void fnRateFitness();
-void fnMutatePop();
-void fnCrossoverPop();
-void fnInitCycle();
-virtual std::ostream& print(std::ostream& o) const;
-
-};
-
-
- 
-Population ::~Population() {
-        for(size_t i = 0; i < m_cPopulationCount; i++) {
-                delete m_rgcbPopulation[i];
-        }
-        delete [] m_rgcbPopulation;
-        delete m_cbBestInd;
-}
-
-/*
- * Constructor - Fills member Array with n (TODO: 50?!)
- */
-Population ::Population(size_t ucInitialPopSize, size_t boardsize) {
-        m_rgcbPopulation = new ChessBoard *[ucInitialPopSize]; //array initialize with ucInitialPopSize
-        m_cPopulationCount = 0;
-		m_boardSize = boardsize;
-
-        std::cout << m_cPopulationCount << std::endl;
-        for(size_t i = 0; i < ucInitialPopSize; i++) {
-                m_rgcbPopulation[i] = new ChessBoard(m_boardSize);
-               // std::cout << m_rgcbPopulation[i]->m_iFitness << std::endl;
-                //m_cPopulationCount++;
-        }
-        m_cPopulationCount = ucInitialPopSize;
-		m_bestPop = m_cPopulationCount/6;
-		fnRateFitness();
-		fnInitCycle();
-}
-
-/*
- * Rates Fitness for Individuals in Population
- */
-void Population ::fnRateFitness() {
-
-
-        for(size_t i = 0; i < m_cPopulationCount; i++) {
-            m_rgcbPopulation[i]->fnCheckFitness();
-        }
-		int min;
-		size_t nextmin;
-		for (size_t i = 0; i < m_cPopulationCount; i++)
-		{
-			min = m_rgcbPopulation[i]->m_iFitness;
-			nextmin = 0;
-			for (size_t j = i+1; j < m_cPopulationCount; j++)
-			{
-				if (min > m_rgcbPopulation[j]->m_iFitness)
-				{
-					nextmin = j;
-					min = m_rgcbPopulation[j]->m_iFitness;
-				}
-			}
-			if (nextmin != 0)
-			{
-				ChessBoard* temp = m_rgcbPopulation[i];
-				m_rgcbPopulation[i] = m_rgcbPopulation[nextmin];
-				m_rgcbPopulation[nextmin] = temp;
-			}
 		}
 
+		int fnCheck();
+
+	};
+
+	/*
+	* Dynamic array that holds the Indiviuals (TODO: use vector instead?)
+	*/
+	ChessBoard **m_cbBestInd;
+	const static size_t m_cMaxPopulation = 500;
+	size_t m_boardSize;
+	size_t m_cPopulationCount;
+	size_t m_bestPop;
+	ChessBoard **m_rgcbPopulation;
+
+public:
+	Population(size_t, size_t);
+	~Population();
+	void fnRateFitness();
+	void fnMutatePop();
+	void fnCrossoverPop();
+	void fnInitCycle();
+	virtual std::ostream& print(std::ostream& o) const;
+
+};
+
+
+
+Population ::~Population() {
+	for (size_t i = 0; i < m_cPopulationCount; i++) {
+		delete m_rgcbPopulation[i];
+	}
+	delete[] m_rgcbPopulation;
+	delete m_cbBestInd;
 }
 
 /*
- * Checks if a queen is the only one in a vertical/horizontal row
- * counter to modify fitness var in Individuals
- */
-int Population ::ChessBoard::fnCheck() {
-        int i_cFitCount = 0;
+* Constructor - Fills member Array with n (TODO: 50?!)
+*/
+Population::Population(size_t ucInitialPopSize, size_t boardsize) {
+	std::cout << std::endl << std::endl << "Initialising population of " << ucInitialPopSize << " individuals..." << std::endl << std::endl;
+	m_rgcbPopulation = new ChessBoard *[ucInitialPopSize]; //array initialize with ucInitialPopSize
+	m_cPopulationCount = 0;
+	m_boardSize = boardsize;
+	for (size_t i = 0; i < ucInitialPopSize; i++) {
+		m_rgcbPopulation[i] = new ChessBoard(m_boardSize);
 
-        for(size_t i = 0; i < m_boardSize; i++) {
-			int pos = m_rgQueenPositions[i];
-			int counter = 1;
-                for(size_t j = i+1; j < m_boardSize; j++) {
-					int testPos = m_rgQueenPositions[j];
-                        if(pos == testPos) {
-                                i_cFitCount++;
-                        }
-						if (pos == testPos + counter)
-						{
-							i_cFitCount++;
-						}
+	}
+	m_cPopulationCount = ucInitialPopSize;
+	m_bestPop = boardsize;
+	fnRateFitness();
+	fnInitCycle();
+}
 
-						if (pos == testPos - counter)
-						{
-							i_cFitCount++;
-						}
-						counter++;
-                }
-        }
+/*
+* Rates Fitness for Individuals in Population
+*/
+void Population::fnRateFitness() {
 
-        return i_cFitCount;
+
+	for (size_t i = 0; i < m_cPopulationCount; i++) {
+		m_rgcbPopulation[i]->fnCheckFitness();
+	}
+
+	for (size_t i = 1; i < m_cPopulationCount; i++)
+	{
+		size_t j = i;
+
+		while(j>0&& m_rgcbPopulation[j]->m_iFitness<m_rgcbPopulation[j-1]->m_iFitness)
+		{
+			ChessBoard* temp = m_rgcbPopulation[j];
+			m_rgcbPopulation[j] = m_rgcbPopulation[j-1];
+			m_rgcbPopulation[j-1] = temp;
+			j--;
+		}
+	}
+
+}
+
+/*
+* Checks if a queen is the only one in a vertical/horizontal row
+* counter to modify fitness var in Individuals
+*/
+int Population::ChessBoard::fnCheck() {
+	int i_cFitCount = 0;
+
+	for (size_t i = 0; i < m_boardSize; i++) {
+		int pos = m_rgQueenPositions[i];
+		int counter = 1;
+		for (size_t j = i + 1; j < m_boardSize; j++) {
+			int testPos = m_rgQueenPositions[j];
+			if (pos == testPos) {
+				i_cFitCount++;
+			}
+			if (pos == testPos + counter)
+			{
+				i_cFitCount++;
+			}
+
+			if (pos == testPos - counter)
+			{
+				i_cFitCount++;
+			}
+			counter++;
+		}
+	}
+
+	return i_cFitCount;
 }
 
 
- 
 
 
- 
-void Population ::fnMutatePop() {
+
+
+void Population::fnMutatePop() {
 
 
 	for (size_t i = m_bestPop; i < m_cPopulationCount; i++)
@@ -185,56 +173,73 @@ void Population ::fnMutatePop() {
 	}
 }
 
- 
-void Population ::fnCrossoverPop() {
 
-
-	for (size_t i = 0; i < m_cPopulationCount - m_bestPop; i++)
+void Population::fnCrossoverPop() {
+	for (size_t i = 1; i < m_cPopulationCount - m_bestPop; i++)
 	{
 		int dec = 1;
 		if (i < m_bestPop);
-		else if (i < m_cPopulationCount / 4)
+		else if (i < m_cPopulationCount / 8)
+		{
 			dec = rand() % 2;
-		else if (i < m_cPopulationCount / 2)
+		}
+		else if (i < m_cPopulationCount / 4)
 		{
 			dec = rand() % 3;
 		}
+		else if (i < m_cPopulationCount / 2)
+		{
+			dec = rand() % 6;
+		}
 		else if (i < m_cPopulationCount / 1.5)
 		{
-			dec = rand() % 5;
+			dec = rand() % 8;
 		}
 		else
 		{
-			dec = 0;
+			dec = rand() % 12;
 		}
-			if (dec == 1)
+		if (dec == 1)
+		{
+			size_t rand1 = rand() % (m_cPopulationCount - m_bestPop);
+			size_t rand2 = rand() % m_boardSize;
+			if (rand2 > m_boardSize / 2)
 			{
-				size_t rand1 = rand() % (m_cPopulationCount/2);
-				size_t rand2 = rand() % m_boardSize;
-				/*if (rand2 > m_boardSize / 2)
+				for (size_t j = m_boardSize; j > rand2; j--)
 				{
-					for (size_t j = m_boardSize; j > rand2; j--)
+					size_t rand3 = rand() % m_boardSize;
+					if (rand1 != 0)
 					{
-						size_t temp = m_rgcbPopulation[i]->m_rgQueenPositions[j];
-						m_rgcbPopulation[i]->m_rgQueenPositions[j] = m_rgcbPopulation[rand1]->m_rgQueenPositions[j];
-						m_rgcbPopulation[rand1]->m_rgQueenPositions[j] = temp;
+						size_t temp = m_rgcbPopulation[i]->m_rgQueenPositions[rand3];
+						m_rgcbPopulation[i]->m_rgQueenPositions[rand3] = m_rgcbPopulation[rand1]->m_rgQueenPositions[rand3];
+						m_rgcbPopulation[rand1]->m_rgQueenPositions[rand3] = temp;
+					}
+					else
+					{
+						m_rgcbPopulation[i]->m_rgQueenPositions[rand3] = m_rgcbPopulation[rand1]->m_rgQueenPositions[rand3];
 					}
 
 				}
-				else
-				{
-					for (size_t j = rand2; j < m_boardSize / 2; j++)
-					{
-						size_t temp = m_rgcbPopulation[i]->m_rgQueenPositions[j];
-						m_rgcbPopulation[i]->m_rgQueenPositions[j] = m_rgcbPopulation[rand1]->m_rgQueenPositions[j];
-						m_rgcbPopulation[rand1]->m_rgQueenPositions[j] = temp;
-					}
-				}*/
-				size_t rand3 = rand() % m_boardSize;
-				size_t temp = m_rgcbPopulation[i]->m_rgQueenPositions[rand3];
-				m_rgcbPopulation[i]->m_rgQueenPositions[rand3] = m_rgcbPopulation[rand1]->m_rgQueenPositions[rand3];
-				m_rgcbPopulation[rand1]->m_rgQueenPositions[rand3] = temp;
 			}
+			else
+			{
+				for (size_t j = rand2; j < m_boardSize / 2; j++)
+				{
+					size_t rand3 = rand() % m_boardSize;
+					if (rand1 != 0)
+					{
+						size_t temp = m_rgcbPopulation[i]->m_rgQueenPositions[rand3];
+						m_rgcbPopulation[i]->m_rgQueenPositions[rand3] = m_rgcbPopulation[rand1]->m_rgQueenPositions[rand3];
+						m_rgcbPopulation[rand1]->m_rgQueenPositions[rand3] = temp;
+					}
+					else
+					{
+						m_rgcbPopulation[i]->m_rgQueenPositions[rand3] = m_rgcbPopulation[rand1]->m_rgQueenPositions[rand3];
+					}
+				}
+			}
+
+		}
 	}
 }
 
@@ -244,41 +249,44 @@ void Population::fnInitCycle()
 	int counter = 1;
 	while (m_rgcbPopulation[0]->m_iFitness > 0)
 	{
-
 		fnCrossoverPop();
 		fnMutatePop();
 		fnRateFitness();
 		counter++;
-		if (counter % 1000 == 0)
+		if (counter % 5000 == 0)
 		{
-			std::cout << m_rgcbPopulation[0]->m_iFitness<<std::endl;
+			std::cout << "Best current fitness (after 5000 generations):  " << m_rgcbPopulation[0]->m_iFitness << std::endl;
 		}
 	}
-	std::cout << "cycle: " << counter<<std::endl;
+	std::cout <<std::endl<< "Generation: " << counter << std::endl<<std::endl;
 }
 
-std::ostream& Population ::print(std::ostream& o) const {
-        o << "Population, N = " << m_boardSize << ", popCount = " << m_cPopulationCount <<  '\n';
-        for(size_t i = 0; i < 2; i++) {
-                o << '\n';
-                o << "Individual " << i << ", fitness = " << m_rgcbPopulation[i]->m_iFitness << '\n';
-                o << "QueensPos: [";
-                for(size_t j = 0; j < m_boardSize; j++) {
-                        o << m_rgcbPopulation[i]->m_rgQueenPositions[j] << ' ';
-                } o << ']' << '\n';
-        }
-		for (size_t i = 0; i < m_boardSize; i++)
-		{
-			
-			for (size_t j = 0; j < m_boardSize; j++)
+std::ostream& Population::print(std::ostream& o) const {
+	o << "Population, N = " << m_boardSize << ", popCount = " << m_cPopulationCount << '\n';
+	for (size_t i = 0; i < 1; i++) {
+		o << '\n';
+		o << "Individual with fitness = " << m_rgcbPopulation[i]->m_iFitness << '\n';
+		o << "QueensPos: [";
+		for (size_t j = 0; j < m_boardSize; j++) {
+			o << m_rgcbPopulation[i]->m_rgQueenPositions[j] ;
+			if (j != m_boardSize - 1)
 			{
-				if (m_rgcbPopulation[0]->m_rgQueenPositions[i] == j)
-					o << 1<<" ";
-				else
-					o << 0<<" ";
+				o << ", ";
 			}
-			o<< std::endl;
-		}
+		} o << ']' << '\n'<< '\n';
+	}
+	for (size_t i = 0; i < m_boardSize; i++)
+	{
 
-        return o;
+		for (size_t j = 0; j < m_boardSize; j++)
+		{
+			if (m_rgcbPopulation[0]->m_rgQueenPositions[i] == j)
+				o << 1 << " ";
+			else
+				o << 0 << " ";
+		}
+		o << std::endl;
+	}
+
+	return o;
 }
--- nQueensProblem/nQueensProblem.cpp
+++ nQueensProblem/nQueensProblem.cpp
@@ -7,14 +7,44 @@
 #include "Population.h"
 
 int main() {
-		
-	size_t n = 8;
-	size_t pop = 1000;// n * 25;
-        Population* p = new Population(pop, n);
+	int dec = 1;
+	
+	while (dec == 1)
+	{
+		size_t n = 8;
+		std::cout << "1  -  8-queens problem" << std::endl;
+		std::cout << "2  -  Variable n-queens problem" << std::endl;
+		int dec1;
+		std::cin >> dec1;
+		if (dec1 != 1 && dec1 != 2)
+		{
+			dec1 = 1;
+			std::cout << "Invalid value!  Initiating 8-queens problem.";
+		}
+		else if (dec1 == 2)
+		{
+			std::cout << "Enter value n." << std::endl;
+			std::cin >> n;
+		}
 
-        p->print(std::cout);
-		int no;
-		std::cin >> no;
+
+		size_t pop =  n * 50;
+		Population* p = new Population(pop, n);
+
+		p->print(std::cout);
+		std::cout << std::endl << std::endl << "1  -  Reset" << std::endl << "2  -  Exit"<<std::endl;
+		int dec2;
+		std::cin >> dec2;
+		if (dec2 == 2)
+		{
+			dec= 0;
+		}
+		else
+		{
+			std::cout << std::endl << std::endl<<std::endl;
+		}
+	}
+	
 
 
         return 0;
